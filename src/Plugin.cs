fd7b14a75c50659bda956eeb24529553179500f3
warpersan <leumas.ecole@gmail.com>
2026-02-22 22:00:29 -0500

    Removed `RiskOfOptions` :(

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -4,14 +4,11 @@ using WhatchaGotThere.Helpers;
 namespace WhatchaGotThere;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-// Soft
-[BepInDependency(RiskOfOptions.PluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.SoftDependency)]
 internal class Plugin : BaseUnityPlugin
 {
 	private void Awake()
 	{
 		Configuration.Load(Config);
-		Helpers.Dependencies.LoadDependencies(Configuration.Instance);
 		Patch.ApplyAll();
 		Log.Info($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
 	}
