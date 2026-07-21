d24cc8de1d1849990375ccf2fcd0ee8fb108cf4c
WarperSan <leumas.ecole@gmail.com>
2026-07-21 16:19:21 -0400

    Added check

--- src/Dependencies/RiskOfOptions/Dependency.cs
+++ src/Dependencies/RiskOfOptions/Dependency.cs
@@ -17,6 +17,12 @@ internal static class Dependency
 	[MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
 	public static void ApplyConfiguration(Configuration? config)
 	{
+		if (config == null)
+		{
+			Log.Warning($"Tried to load the configurations into '{nameof(RiskOfOptions)}', but none were given.");
+			return;
+		}
+
 		throw new NotImplementedException();
 	}
 }
