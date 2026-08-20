f660e5e34d9c728e17f1bc98804edd6f7b8d6e62
WarperSan <leumas.ecole@gmail.com>
2026-08-20 11:01:51 -0400

    Revamped

--- src/API/DisplayHandler.cs
+++ src/API/DisplayHandler.cs
@@ -9,52 +9,24 @@ namespace WhatchaGotThere.API;
 /// </summary>
 public static class DisplayHandler
 {
-	private static readonly List<Func<CharacterMaster, bool?>> DisplayConditions = [];
+	private static readonly List<Func<CharacterMaster, bool?>> DisplayConditions =
+	[
+		DisableIfNoEquipment,
+		DisableIfTargetNotAllowed
+	];
 
 	/// <summary>
 	/// Defines if the equipment icon of the given <see cref="CharacterMaster"/> should be displayed
 	/// </summary>
 	public static bool IsDisplayed(CharacterMaster master)
 	{
-		// If no equipment, disable
-		if (master.inventory.currentEquipmentState.equipmentIndex == EquipmentIndex.None)
-			return false;
-
-		// If no configuration, disable
-		if (Configuration.Instance != null)
-		{
-			var type = Configuration.Instance.AllowedTargets.Value;
-
-			// If no target allowed, disable
-			if (type == Configuration.TargetType.None)
-				return false;
-
-			if (master.hasBody)
-			{
-				var bodyIndex = master.GetBody().bodyIndex;
-
-				// If master is a survivor, use survivors target
-				if (SurvivorCatalog.GetSurvivorIndexFromBodyIndex(bodyIndex) != SurvivorIndex.None)
-					return type.HasFlag(Configuration.TargetType.Survivors);
-
-				// If master is a drone, use drones target
-				if (DroneCatalog.GetDroneIndexFromBodyIndex(bodyIndex) != DroneIndex.None)
-					return type.HasFlag(Configuration.TargetType.Drones);
-
-				// If master is an ally, use allies target
-				if (master.minionOwnership.ownerMaster != null)
-					return type.HasFlag(Configuration.TargetType.Allies);
-			}
-		}
-
-		// If any condition enable, enable
 		foreach (var condition in DisplayConditions)
 		{
 			var result = condition.Invoke(master);
-			
+
 			if (!result.HasValue)
 				continue;
-			
+
 			return result.Value;
 		}
 
@@ -64,9 +36,55 @@ public static class DisplayHandler
 	/// <summary>
 	/// Adds the given condition to the list of conditions
 	/// </summary>
+	/// <remarks>
+	///	If <see langword="null"/> is returned, the next condition will be used. Otherwise, the result will be used.
+	/// </remarks>
 	[PublicAPI]
 	public static void AddCondition(Func<CharacterMaster, bool?> condition)
 	{
 		DisplayConditions.Add(condition);
 	}
+
+	#region Built-in
+
+	private static bool? DisableIfNoEquipment(CharacterMaster master)
+	{
+		if (master.inventory.currentEquipmentState.equipmentIndex == EquipmentIndex.None)
+			return false;
+
+		return null;
+	}
+
+	private static bool? DisableIfTargetNotAllowed(CharacterMaster master)
+	{
+		if (Configuration.Instance == null)
+			return null;
+
+		var type = Configuration.Instance.AllowedTargets.Value;
+
+		// If no target allowed, disable
+		if (type == Configuration.TargetType.None)
+			return false;
+
+		if (!master.hasBody)
+			return null;
+
+		var bodyIndex = master.GetBody().bodyIndex;
+
+		// If master is a survivor, use survivors target
+		if (SurvivorCatalog.GetSurvivorIndexFromBodyIndex(bodyIndex) != SurvivorIndex.None)
+			return type.HasFlag(Configuration.TargetType.Survivors);
+
+		// If master is a drone, use drones target
+		if (DroneCatalog.GetDroneIndexFromBodyIndex(bodyIndex) != DroneIndex.None)
+			return type.HasFlag(Configuration.TargetType.Drones);
+
+		// If master is an ally, use allies target
+		if (master.minionOwnership.ownerMaster != null)
+			return type.HasFlag(Configuration.TargetType.Allies);
+
+		return null;
+	}
+
+	#endregion
 }
